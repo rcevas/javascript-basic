21ab39e1a442cc1ac9d648e93a41c5e7a71ac55f
Rodrigo Cevas <rcevas@gmail.com>
2019-10-09 12:32:24 +0200

    suma

--- script.js
+++ script.js
@@ -4,3 +4,9 @@ alert("Me llamo Rod");
 
 var nombre = "Rodrigo";
 var apellido = "Cevas";
+
+var a = 5;
+var b = 8;
+var suma = a + b;
+var total = "la suma entre " + a + " y " + b + " es " + suma;
+console.log(total);
